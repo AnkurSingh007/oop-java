878ee4ca80319b758298d36d473f78cd90e2f732
Ankur Singh <singh_a@worksap.co.jp>
2019-04-09 07:48:46 +0800

    chapter_2 Added deck class design

---
+++ chapter_2/Deck.java
@@ -0,0 +1,46 @@
+class Deck{
+    /**
+     * Constructor: Create a shuffled deck of cards
+     * precondition: None
+     * postcondition: A Deck of 52 cards
+    */
+    public Deck(){
+    	
+    }
+    
+    /** Return the number of cards present in the deck
+     * @return Number of cards in the deck of cards
+     * precondition: none
+     * postcondition: The deck is unchanged
+     */
+    public int size(){
+    	
+    }
+    
+    /** Determine if this deck is empty
+     * @return If deck is empty or not
+     * precondition: None
+     * postcondition: Deck is unchanged
+     */
+    public boolean isEmpty(){
+    	
+    }
+    
+    /**
+     * Shuffle a deck of cards
+     * precondition: None
+     * postcondition: The existing deck of cards with the cards in random order
+     */
+    public void shuffle(){
+    	
+    }
+    
+    /**
+     * @return a card from the deck of cards
+     * precondition: Deck is non empty
+     * postcondtion: Deck has one less card
+     */
+    public Card deal(){
+    	
+    }
+}
